9e9d1c4a9e67c28873f6be662b878bdd908192ac
OgiBalboa <48028006+OgiBalboa@users.noreply.github.com>
2019-09-12 10:55:56 +0300

    Add files via upload

---
+++ game/ui/oyuncu.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 01:34:35 2019
+
+@author: fener_000
+"""
+
+class oyuncu():
+    def __init__(self,name,nu,avatar):
+        self.name = name
+        self.nu = nu #oyuncu no su
+        self.avatar = avatar
+    
+    
+        
